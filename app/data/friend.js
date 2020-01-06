3f0c30bd05e2104821c20560c7e4987ae440aafc
Kassandra Gonzalez <kgkassieg@gmail.com>
2020-01-06 10:45:46 -0600

    created object for friends varia

---
+++ app/data/friend.js
@@ -0,0 +1,33 @@
+var friends = [
+  (
+    name: "Jim Halpert",
+    photo: "image", 
+    scores: [5, 1, 2, 3, 4, 4, 5, 3, 2, 1, 3]
+  ),
+
+  (
+    name: "Jim Halpert",
+    photo: "image",
+    scores: [5, 1, 2, 3, 4, 4, 5, 3, 2, 1, 3]
+  ),
+
+  (
+    name: "Jim Halpert",
+    photo: "image",
+    scores: [5, 1, 2, 3, 4, 4, 5, 3, 2, 1, 3]
+  ),
+
+  (
+    name: "Jim Halpert",
+    photo: "image",
+    scores: [5, 1, 2, 3, 4, 4, 5, 3, 2, 1, 3]
+  ),
+
+  (
+    name: "Jim Halpert",
+    photo: "image",
+    scores: [5, 1, 2, 3, 4, 4, 5, 3, 2, 1, 3]
+  )
+];
+
+module.exports = friends; 
