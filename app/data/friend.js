8766ef4663cda92ded7111b73067292dec4a6249
Kassandra Gonzalez <kgkassieg@gmail.com>
2020-01-06 11:00:33 -0600

    updated friends name

--- app/data/friend.js
+++ app/data/friend.js
@@ -2,31 +2,31 @@ var friends = [
   (
     name: "Jim Halpert",
     photo: "image", 
-    scores: [5, 1, 2, 3, 4, 4, 5, 3, 2, 1, 3]
+    scores: [5, 1, 2, 3, 4, 4, 5, 3, 5, 3]
   ),
 
   (
-    name: "Jim Halpert",
+    name: "Michael Scott",
     photo: "image",
-    scores: [5, 1, 2, 3, 4, 4, 5, 3, 2, 1, 3]
+    scores: [3, 1, 1, 4, 4, 4, 2, 2, 4, 1]
   ),
 
   (
-    name: "Jim Halpert",
+    name: "Pam Beesly",
     photo: "image",
-    scores: [5, 1, 2, 3, 4, 4, 5, 3, 2, 1, 3]
+    scores: [4, 2, 2, 2, 3, 1, 1, 1, 3, 4]
   ),
 
   (
-    name: "Jim Halpert",
+    name: "Dwight Schrute",
     photo: "image",
-    scores: [5, 1, 2, 3, 4, 4, 5, 3, 2, 1, 3]
+    scores: [2, 3, 3, 3, 3, 1, 5, 4, 2, 2]
   ),
 
   (
-    name: "Jim Halpert",
+    name: "Kelly Kapoor",
     photo: "image",
-    scores: [5, 1, 2, 3, 4, 4, 5, 3, 2, 1, 3]
+    scores: [1, 5, 5, 5, 5, 4, 6, 5, 1, 5]
   )
 ];
 
